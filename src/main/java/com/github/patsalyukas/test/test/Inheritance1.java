8683668b0eaf8aa096e66c2f4cfac160ad2fc2bb
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-05-30 23:01:49 +0300

    HackerRank exercise Inheritance1

---
+++ src/main/java/com/github/patsalyukas/test/test/Inheritance1.java
@@ -0,0 +1,13 @@
+package com.github.patsalyukas.test.test;
+
+public class Inheritance1 {
+    class Arithmetic {
+        public int add(int a, int b) {
+            return a + b;
+        }
+    }
+
+    class Adder extends Arithmetic {
+
+    }
+}
