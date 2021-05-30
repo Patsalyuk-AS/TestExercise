deaa1b5d2c41b27a999162a159a45143737c9174
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-05-30 23:24:06 +0300

    Test Main

--- src/main/java/com/github/patsalyukas/test/Main.java
+++ src/main/java/com/github/patsalyukas/test/Main.java
@@ -1,16 +1,39 @@
 package com.github.patsalyukas.test;
 
 
+import jdk.nashorn.internal.objects.annotations.Function;
+
 import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.util.*;
+import java.util.stream.IntStream;
 import java.util.stream.Stream;
+
+interface AdvancedArithmetic{
+    int divisor_sum(int n);
+}
+
+class MyCalculator implements AdvancedArithmetic{
+    @Override
+    public int divisor_sum(int n) {
+        int sum = 0;
+        for (int i = 1; i <= n / 2; i++) {
+            if (n % i == 0) {
+                sum+=i;
+            }
+        }
+        return sum;
+    }
+}
 
 public class Main {
 
     public static void main(String[] args) throws ClassNotFoundException{
-        Stream<Integer> stream =  Arrays.asList(1, 2, 3, 5).stream();
-        Integer integer = stream.max(Comparator.comparingInt(Integer::intValue)).orElse(1);
+        int n = 6;
+        int i = 0;
+
+        int sum =  Stream.iterate(1, k -> k + 1).limit(n).filter(k -> n%k==0).mapToInt(k -> k).sum();
+        System.out.println(sum);
 
     }
 }
