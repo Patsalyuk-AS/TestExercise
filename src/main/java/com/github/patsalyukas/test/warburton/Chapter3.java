5b5bae068e44f4789ba9365361580e3790ee2b43
Andrey Patsalyuk <pazalyuk@gmail.com>
2021-06-30 15:57:28 +0300

    Warburton Lambda cp3 1.3

--- src/main/java/com/github/patsalyukas/test/warburton/Chapter3.java
+++ src/main/java/com/github/patsalyukas/test/warburton/Chapter3.java
@@ -7,13 +7,18 @@ import java.util.stream.Stream;
 public class Chapter3 {
 
     //1.1
-    int addUp(Stream<Integer> numbers) {
+    public int addUp(Stream<Integer> numbers) {
         return numbers.mapToInt(e -> e).sum();
     }
 
     //1.2
     List<String> getNamesAndPlaces(Stream<Artist> artists) {
         return (artists.map(artist -> artist.getName() + "-" + artist.getOrigin()).collect(Collectors.toList()));
+    }
+
+    //1.3
+    public List<Album> albumWithThreeAndMoreTracks(Stream<Album> albums) {
+        return albums.filter(e -> e.getTracks().size() > 2).collect(Collectors.toList());
     }
 
 }
--- src/test/java/com/github/patsalyukas/test/warburton/Chapter3Test.java
+++ src/test/java/com/github/patsalyukas/test/warburton/Chapter3Test.java
@@ -2,7 +2,11 @@ package com.github.patsalyukas.test.warburton;
 
 import org.junit.jupiter.api.Test;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +29,25 @@ class Chapter3Test {
         Artist artist2 = new Artist("Vera Brezhneva", "Russia");
         Artist artist3 = new Artist("John Lennon", new HashSet<>(), "UK");
         Stream<Artist> artists = Stream.of(artist1, artist2, artist3);
-        System.out.println(cp3.getNamesAndPlaces(artists));
+        List<String> list = cp3.getNamesAndPlaces(artists);
+        assertEquals("Madonna-USA", list.get(0));
+        assertEquals("Vera Brezhneva-Russia", list.get(1));
+        assertEquals("John Lennon-UK", list.get(2));
+    }
+
+    @Test
+    void albumWithThreeAndMoreTracks() {
+        List<Track> tracksEmpty = new ArrayList<>();
+        List<Track> tracksWith2Songs = Arrays.asList(new Track("Song1"), new Track("Song2"));
+        List<Track> tracksWith3Songs = Arrays.asList(new Track("Song1"), new Track("Song2"), new Track("Song3"));
+        List<Track> tracksWith4Songs = Arrays.asList(new Track("Song1"), new Track("Song2"), new Track("Song3"), new Track("Song4"));
+        Album album1 = new Album("album1", tracksEmpty, null);
+        Album album2 = new Album("album2", tracksWith2Songs, null);
+        Album album3 = new Album("album3", tracksWith3Songs, null);
+        Album album4 = new Album("album4", tracksWith4Songs, null);
+        List<Album> list = cp3.albumWithThreeAndMoreTracks(Stream.of(album1, album2, album3, album4));
+        assertEquals(2, cp3.albumWithThreeAndMoreTracks(Stream.of(album1, album2, album3, album4)).size());
+
+
     }
 }
